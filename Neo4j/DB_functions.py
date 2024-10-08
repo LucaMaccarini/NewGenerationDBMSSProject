70f6c8bcd4843db4fb38b2605c2b07fca9554481
Luca Maccarini <lucamaccarini22@gmail.com>
2024-10-09 00:36:56 +0200

    load from csv

--- Neo4j/DB_functions.py
+++ Neo4j/DB_functions.py
@@ -1,11 +1,47 @@
 import os
+import config
 from neo4j import GraphDatabase
 
 def get_neo4j_connection():
     uri = os.getenv('NEO4J_URI')
-    user = os.getenv('NEO4J_USER')
+    user = os.getenv('NEO4J_USERNAME')
     password = os.getenv('NEO4J_PASSWORD')
+
     return GraphDatabase.driver(uri, auth=(user, password))
+
+def close_neo4j_connection(driver):
+    driver.close()
+
+def load_customers_from_csv():
+    driver = get_neo4j_connection()
+    
+    # Usa il link diretto per scaricare il CSV
+    csv_file_url = "https://drive.google.com/uc?id=1aOAeJb7LY41sqekzPdL3bJx1omQeKc0H"
+    
+    query = f"""
+    CALL apoc.periodic.iterate(
+        'LOAD CSV WITH HEADERS FROM "{csv_file_url}" AS row RETURN row',
+        'MERGE (c:Customer {{customer_id: row.CUSTOMER_ID}})
+         ON CREATE SET 
+            c.x_customer_id = toFloat(row.x_customer_id),
+            c.y_customer_id = toFloat(row.y_customer_id),
+            c.mean_amount = toFloat(row.mean_amount),
+            c.std_amount = toFloat(row.std_amount),
+            c.mean_nb_tx_per_day = toFloat(row.mean_nb_tx_per_day)
+        ',
+        {{batchSize: {config.lines_per_commit}, parallel: true}}
+    )
+    """
+
+    try:
+        with driver.session() as session:
+            result = session.run(query)
+    except Exception as e:
+        print(f"ERROR load_customers_from_csv: {e}")
+    finally:
+        close_neo4j_connection(driver)
+
+
 
 def create_customer(customer_id, x_customer_id, y_customer_id, mean_amount, std_amount, mean_nb_tx_per_day, available_terminals):
     driver = get_neo4j_connection()
--- Neo4j/config.py
+++ Neo4j/config.py
@@ -1,2 +1,4 @@
 # cartella contenente il db generato, ovvero i 3 csv creati dallo script di generazione
-import_DB_absolute_path = "/home/macca/Scrivania/new generation/progetto/NewGenerationDBMSSProject/Generated_DBs/Test1"
+import_DB_absolute_path = "/home/macca/Scrivania/Collegamento a unimi/new generation/progetto/NewGenerationDBMSSProject/Generated_DBs/Test1"
+customers_csv = 'customers.csv'
+lines_per_commit = 1000
