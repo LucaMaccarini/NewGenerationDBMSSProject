60aae717e3a847e18866d6aa4661c3af1a2a42eb
luca.maccarini <luca.maccarini@grupposet.it>
2024-12-04 17:54:56 +0100

    almost done query a1 and a2 fix translations

--- GenerationScript/config.py
+++ GenerationScript/config.py
@@ -1,7 +1,7 @@
 DBs = [
    {
        "DB_name": "Small-DB-1",
-       "n_customers": 500,
+       "n_customers": 1000,
        "n_terminals": 300,
        "n_days": 7,
        "start_date": '2024-12-30',
