35d3621bac9c0d4327ea9654b39bdad5b5678b3a
luca.maccarini <luca.maccarini@grupposet.it>
2024-12-04 17:59:46 +0100

    config generation dbs

--- GenerationScript/config.py
+++ GenerationScript/config.py
@@ -1,18 +1,42 @@
 DBs = [
-   {
-       "DB_name": "Small-DB-1",
-       "n_customers": 1000,
-       "n_terminals": 300,
-       "n_days": 7,
-       "start_date": '2024-12-30',
-       "radius": 10
-    },
     {
-        "DB_name": "Small-DB-2",
+        "DB_name": "DB1",
         "n_customers": 1000,
         "n_terminals": 500,
-        "n_days": 14,
-        "start_date": '2024-01-01',
+        "n_days": 365,  # 1 anno
+        "start_date": '2025-01-01',
+        "radius": 5
+    },
+    {
+        "DB_name": "DB2",
+        "n_customers": 1000,
+        "n_terminals": 500,
+        "n_days": 730,  # 2 anni
+        "start_date": '2025-01-01',
+        "radius": 5
+    },
+    {
+        "DB_name": "DB3",
+        "n_customers": 1000,
+        "n_terminals": 500,
+        "n_days": 1095,  # 3 anni
+        "start_date": '2025-01-01',
+        "radius": 5
+    },
+    {
+        "DB_name": "DB4",
+        "n_customers": 1000,
+        "n_terminals": 500,
+        "n_days": 1460,  # 4 anni
+        "start_date": '2025-01-01',
+        "radius": 5
+    },
+    {
+        "DB_name": "DB5",
+        "n_customers": 1000,
+        "n_terminals": 500,
+        "n_days": 1825,  # 5 anni
+        "start_date": '2025-01-01',
         "radius": 5
     }
 ]
