b94fcf9c766a22b86a4fcdddfe405d687bc7ff18
Luca Maccarini <lucamaccarini22@gmail.com>
2024-12-08 21:53:44 +0100

    add

--- GenerationScript/config.py
+++ GenerationScript/config.py
@@ -1,17 +1,17 @@
 DBs = [
-    #{
-    #    "DB_name": "50MB",
-    #    "n_customers": 1000,
-    #    "n_terminals": 500,
-    #    "n_days": 500,
-    #    "start_date": '2022-01-01',
-    #    "radius": 5
-    #},
+    {
+        "DB_name": "50MB",
+        "n_customers": 1000,
+        "n_terminals": 500,
+        "n_days": 500,
+        "start_date": '2022-01-01',
+        "radius": 5
+    },
     {
         "DB_name": "100MB",
         "n_customers": 1200,
-        "n_terminals": 500,
-        "n_days": 700,
+        "n_terminals": 600,
+        "n_days": 800,
         "start_date": '2022-01-01',
         "radius": 5
     },
