991daa0649ea1f3a58d3ad8e486f40c09307da53
luca.maccarini <luca.maccarini@grupposet.it>
2024-12-15 18:42:18 +0100

    dix notebook

--- GenerationScript/generate_datasets_csv.py
+++ GenerationScript/generate_datasets_csv.py
@@ -13,9 +13,9 @@ from generate_dataset import generate_dataset
 
 
 
-# Loop sui DB definiti nel file di configurazione
+# Loop through the databases defined in the configuration file
 for db in config.DBs:
-    # Generazione delle tabelle del DB usando i valori di configurazione
+    # Generate database tables using configuration values
     (customer_profiles_table, terminal_profiles_table, transactions_df) = generate_dataset(
         n_customers=db["n_customers"], 
         n_terminals=db["n_terminals"], 
@@ -24,29 +24,29 @@ for db in config.DBs:
         r=db["radius"]
     )
 
-    # Aggiungere frodi alle transazioni
+    # Add fraud data to the transactions
     transactions_df = add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df)
 
     
-    # Converto i valori della serie available_terminals dato che gli interi nella lista sono interi numpy
+    # Convert the values of the 'available_terminals' series, as the integers in the list are numpy integers
     customer_profiles_table['available_terminals'] = customer_profiles_table['available_terminals'].apply(
         lambda lst: [int(i) if isinstance(i, np.integer) else i for i in lst] if isinstance(lst, (list, np.array)) else lst
     )
 
-    # Preparazione al salvataggio del DB
+    # Prepare for saving the database
     output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Generated_DBs', db["DB_name"])
 
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Salvataggio dei customers
+    # Saving customers
     customer_profiles_table.to_csv(output_dir + '/customers.csv', sep=';', encoding='utf-8', index=False)
 
-    # Salvataggio dei terminals
+    # Saving terminals
     terminal_profiles_table.to_csv(output_dir + '/terminals.csv', sep=';', encoding='utf-8', index=False)
 
-    # Salvataggio delle transactions
+    # Saving transactions
     transactions_df.to_csv(output_dir + '/transactions.csv', sep=';', encoding='utf-8', index=False)
 
     print(f"Database data saved in: {os.path.abspath(output_dir)}/\n")
